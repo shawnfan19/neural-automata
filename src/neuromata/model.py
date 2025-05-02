fix loss averaging mistake
use xavier for weight init, update torch adam eps

src/neuromata/model.py
@@ -29,6 +29,8 @@ class CAModel(torch.nn.Module):
             torch.nn.Conv2d(128, self.cfg.channel_n, kernel_size=1),
         )
 
+        torch.nn.init.xavier_uniform_(self.update_rule[0].weight)  # type: ignore
+
         target_layer = self.update_rule[2]
         torch.nn.init.constant_(target_layer.weight, 0)  # type: ignore
         if target_layer.bias is not None:
@@ -65,7 +67,7 @@ class CAModel(torch.nn.Module):
 
         x = x[:, : pad_target.shape[1], :, :]
 
-        return torch.mean(torch.square(x - pad_target))
+        return torch.mean(torch.square(x - pad_target), dim=[1, 2, 3])
 
     def perceive(self, x: torch.Tensor, angle: float = 0.0):
 

src/neuromata/optim.py
@@ -49,9 +49,7 @@ def configure_optimizer(
         raise ValueError(f"Unknown scheduler: {cfg.scheduler}")
 
     optimizer = torch.optim.Adam(
-        model.parameters(),
-        lr=cfg.lr,
-        betas=(cfg.beta1, cfg.beta2),
+        model.parameters(), lr=cfg.lr, betas=(cfg.beta1, cfg.beta2), eps=1e-7
     )
 
     scheduler = torch.optim.lr_scheduler.LambdaLR(